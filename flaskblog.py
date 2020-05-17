3d4ae6744974c3a32f05380be6e40fe7cb923369
HSI CHENG YUN <hsichengyun@138-38-194-130.eduroam.bath.ac.uk>
2020-05-17 08:16:12 +0100

    1/3 of the second vedio

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 # double underscore name is a name of the module. if we run the script with python directly then double underscore name can be equal to double underscore main
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 17, 2020'
+    },
+    {
+        'author': 'ChengYun Hsi',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 18, 2020'
+    }
+]
 
 # the root page "/"
 @app.route("/")
-@app.route("/home")
+@app.route("/home", posts=posts)
 def home():
-    return "<h1>Hello World!</h1>"
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 
 # this part allows us to run the python script directly without run through export FLASK_DEBUG=1 flask run
