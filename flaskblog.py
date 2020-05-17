99951172aef51cc9972593a950d5f4c313f9f663
HSI CHENG YUN <hsichengyun@138-38-194-130.eduroam.bath.ac.uk>
2020-05-17 17:04:46 +0100

    finish the form

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm #this is the forms we made earlier
+
 # double underscore name is a name of the module. if we run the script with python directly then double underscore name can be equal to double underscore main
 app = Flask(__name__)
+# Create a secure cookie
+app.config['SECRET_KEY'] = 'e3320a22f374154e50bb8750f68275c3'
 
 posts = [
     {
@@ -27,6 +31,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 # this part allows us to run the python script directly without run through export FLASK_DEBUG=1 flask run
 if __name__ == '__main__':
