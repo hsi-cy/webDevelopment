695fbd8f7a3875af102315ae11c8013676b407ab
HSI CHENG YUN <hsichengyun@138-38-194-130.eduroam.bath.ac.uk>
2020-05-18 17:11:05 +0100

    still have bugss

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,41 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm #this is the forms we made earlier
 
 # double underscore name is a name of the module. if we run the script with python directly then double underscore name can be equal to double underscore main
 app = Flask(__name__)
 # Create a secure cookie
 app.config['SECRET_KEY'] = 'e3320a22f374154e50bb8750f68275c3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# we can use database as classes
+db = SQLAlchemy(app)
+
+# create a user model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    # posts is a relationship and it runs in the background when the user post their posts
+    # we are referencing the Post class so it's uppercase
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        """How our object is printed when you print it out"""
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    # we are referencing the user table that's why it's lowercase
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
