04e687596c9fdf9d8e5a7b05a8446e1217ba1583
HSI CHENG YUN <hsichengyun@gmail.com>
2020-05-19 11:01:19 +0100

    finish the login

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 # double underscore name is a name of the module. if we run the script with python directly then double underscore name can be equal to double underscore main
 app = Flask(__name__)
 # Create a secure cookie
@@ -8,5 +9,7 @@ app.config['SECRET_KEY'] = 'e3320a22f374154e50bb8750f68275c3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # we can use database as classes
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes
